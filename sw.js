316fd6020937580877d2ede9fa00afbf1c0a39cc
ywand <ywand2002423@gmail.com>
2025-10-24 22:59:48 +0900

    サービスワーカー全体化

--- sw.js
+++ sw.js
@@ -35,3 +35,20 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // キャッシュがあればそれを返す
+      if (response) return response;
+      // なければネットワークから取得してキャッシュにも保存
+      return fetch(event.request).then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
