0aa0e698ddd5c6103c9729cf12874f2e0615d306
ywand <ywand2002423@gmail.com>
2026-01-26 07:17:03 +0900

    環境再構築中のメッセージ表示。

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div><h1>再構築中。しばらくお待ち下さい。</h1></div>
+  )
+}
+
+export default App
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+function Button() {
+  return (
+    <div></div>
+  )
+}
+
+export default Button
