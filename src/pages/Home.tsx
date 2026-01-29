d1f8e76d86bc06c6a791f3c77166ca34480f11f2
ywand <ywand2002423@gmail.com>
2026-01-30 06:35:54 +0900

    Homeのリンクの追加・整理

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,14 +15,37 @@ function Home({ isDark, onToggleTheme }: Props) {
       onToggleTheme={onToggleTheme}
     >
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="/G0001_kaboom_jump.html">カブーンでジャンプ</a></li>
-          <li><a href="/T0001_memo.html">メモ帳</a></li>
-          <li><a href="T0002_Speech.html">文章読み上げ</a></li>
-        </ul>
+        <section>
+          <h2>メイン</h2>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+        </section>
+        <section>
+          <h2>Game</h2>
+          <ul>
+            <li><a href="/G0001_kaboom_jump.html">カブーンでジャンプ</a></li>
+          </ul>
+        </section>
+        <section>
+          <h2>Tool</h2>
+          <ul>
+            <li><a href="/T0001_memo.html">メモ帳</a></li>
+            <li><a href="T0002_Speech.html">文章読み上げ</a></li>
+          </ul>
+        </section>
+        <section>
+          <h2>（参考）外部サイト</h2>
+          <ul>
+            <li><a href="https://www.google.com/search?q=site:pico-piko-nano.netlify.app" target="_blank">Googleで検索</a></li>
+            <li><a href="https://github.com/" target="_blank">github</a></li>
+            <li><a href="https://www.netlify.com/" target="_blank">netlify</a></li>
+            <li><a href="https://qiita.com/" target="_blank">qiita.com</a></li>
+            <li><a href="https://chatgpt.com/" target="_blank">ChatGPT</a></li>
+          </ul>
+        </section>
       </nav>
-    </MainLayout>
+    </MainLayout >
   )
 }
 
