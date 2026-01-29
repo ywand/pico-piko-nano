82f58b485d88bb583c6c37d31812024c4ecffeba
ywand <ywand2002423@gmail.com>
2026-01-30 06:11:42 +0900

    ファビコンの更新とPWAの再導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
   vanillaExtractPlugin(), // ← これ必須
-  ],
+  VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Pico-Piko-Nano",
+      short_name: "PPNano",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#FAFAFA",
+      theme_color: "#FAFAFA",
+      icons: [
+        {
+          src: "/assets/favicon-128.png",
+          sizes: "128x128",
+          type: "image/png",
+          purpose: "any maskable"
+        },
+        {
+          src: "/assets/favicon.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable"
+        }
+      ]
+    },
+    workbox: {
+      globPatterns: ["**/*.{html,js,css,png,svg,ico}"]
+    },
+  }),
+  ]
 })
