359d767e86099c793314e768fbbec70c4f505805
ywand <ywand2002423@gmail.com>
2026-02-02 00:14:51 +0900

    フォント設定の調整。ダークモード時の設定保存についてローカルストレージとの整合性修正。

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import { lightTheme, darkTheme } from "./styles/theme.css";
 import Home from "./pages/Home";
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    console.log("theme1", localStorage.getItem("theme"));
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  const toggleTheme = () => {
+    setIsDark(prev => {
+      const next = !prev;
+
+      localStorage.setItem("theme", next ? "dark" : "light");
+      console.log("theme2", next ? "dark" : "light");
+
+      return next;
+    });
+  };
 
   return (
     <div className={isDark ? darkTheme : lightTheme}>
       <Home
         isDark={isDark}
-        onToggleTheme={() => {
-          setIsDark(vars => !vars);
-          if (isDark) {
-            localStorage.setItem("theme", "dark");
-          } else {
-            localStorage.setItem("theme", "light");
-          }
-          console.log("theme", localStorage.getItem("theme"));
-        }}
+        onToggleTheme={toggleTheme}
       />
     </div>
   );
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -7,11 +7,44 @@ globalStyle("html, body", {
   padding: 0,
   width: "100%",
   height: "100%",
+  fontSize: "clamp(1rem, 1vw, 1rem)",
+  fontFamily: `
+    "Meiryo",
+    "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans",
+    "Noto Sans JP",
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    sans-serif
+  `,
+  lineHeight: 1.6,
+  letterSpacing: "0",
 });
 
 globalStyle("#root", {
   minHeight: "100%",
+  margin: 0,
+  padding: 0,
 });
+
+globalStyle("header", {
+  margin: 0,
+  padding: 0,
+});
+
+globalStyle("h1, h2, h3, h4, h5, h6", {
+  margin: 0,
+  padding: 0,
+  fontSize: "clamp(1rem, 1vw, 1rem)"
+});
+
+globalStyle("ul", {
+  margin: "0 clamp(1.5em, 1.5vw, 1.5rem)",
+  padding: "0 0 clamp(0.5em, 0.5vw, 0.5rem) 0",
+});
+
 
 // 2. リンクの共通設定
 globalStyle("a", {
