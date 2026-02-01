e5a1f420a2d1a734fd97dc78ff54a23d368a169e
ywand <ywand2002423@gmail.com>
2026-02-01 21:39:08 +0900

    mainへのダークモード取得追加。readme.mdの編集 ￥

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { lightTheme, darkTheme } from './styles/theme.css.ts';
 import App from './App.tsx'
+
+//テーマの取得
+const saved = localStorage.getItem("theme");
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const themeClass =
+  saved === "dark" || (!saved && prefersDark)
+    ? darkTheme
+    : lightTheme;
+document.documentElement.classList.add(themeClass);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
