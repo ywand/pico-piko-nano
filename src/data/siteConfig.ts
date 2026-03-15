d67b4255bf8187b00f3dcc0e978a5cc6470ef16a
ywand <ywand2002423@gmail.com>
2026-03-15 14:09:05 +0900

    Next.jsへの移行準備。

--- src/data/siteConfig.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_CONFIG = {
-  title: "Pico-Piko-Nano",
-  description: "ミニゲームよりも小さな自作ゲーム集。",
-};
--- src/styles/layout/GameLayout.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-
-export const wrapper = style({
-  padding: 0,
-  height: "100dvh",
-  overflow: "hidden",
-  minHeight: "100dvh",
-});;
