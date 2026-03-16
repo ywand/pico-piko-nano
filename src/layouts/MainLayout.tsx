509d130f9335dae959423c8a6bc0bc21116214c0
ywand <ywand2002423@gmail.com>
2026-03-16 09:11:56 +0900

    背景色のグラデーション修正

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function MainLayout({ title, children }: Props) {
   return (
-    <div className="grid grid-cols-6 gap-0 w-full bg-gradient-to-b from-gray-950 to-black">
+    <div className="grid grid-cols-6 gap-0 w-full bg-gradient-to-b from-gray-950 to-black min-h-screen">
       <div className="hidden lg:block"></div>
       <div className="col-span-6 lg:col-start-2 lg:col-span-4">
         <MainHeader title={title} />
