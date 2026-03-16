b8fd1cadfcf57eb48b5be6c0a05943a9a0e6b819
ywand <ywand2002423@gmail.com>
2026-03-16 13:24:09 +0900

    pwaのプラグインの最新化

--- next.config.ts
+++ next.config.ts
@@ -1,54 +1,61 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const withPWA = require("next-pwa")({
+const withPWA = withPWAInit({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-
-  runtimeCaching: [
-    {
-      urlPattern: ({ request }: { request: Request }) =>
-        request.destination === "document",
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "pages",
-        networkTimeoutSeconds: 3,
-        expiration: {
-          maxEntries: 50,
+  workboxOptions: {
+    disableDevLogs: true,
+    maximumFileSizeToCacheInBytes: 7 * 1024 * 1024,
+    runtimeCaching: [
+      {
+        urlPattern: ({ request }: { request: Request }) =>
+          request.destination === "document",
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "pages",
+          networkTimeoutSeconds: 3,
+          expiration: {
+            maxEntries: 50,
+          },
         },
       },
-    },
-    {
-      urlPattern: /^\/_next\/static\//,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "next-static",
-      },
-    },
-    {
-      urlPattern: ({ request }: { request: Request }) =>
-        request.destination === "script" || request.destination === "style",
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-resources",
-      },
-    },
-    {
-      urlPattern: ({ request }: { request: Request }) =>
-        request.destination === "image",
-      handler: "CacheFirst",
-      options: {
-        cacheName: "images",
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24 * 30,
+      {
+        urlPattern: /^\/_next\/static\//,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-static",
         },
       },
-    },
-  ],
+      {
+        urlPattern: ({ request }: { request: Request }) =>
+          request.destination === "script" || request.destination === "style",
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-resources",
+        },
+      },
+      {
+        urlPattern: ({ request }: { request: Request }) =>
+          request.destination === "image",
+        handler: "CacheFirst",
+        options: {
+          cacheName: "images",
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30,
+          },
+        },
+      },
+    ],
+  },
 });
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
 
 export default withPWA(nextConfig);
